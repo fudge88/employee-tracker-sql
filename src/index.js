dc28f9e9292e674c8140ce86c0bf377c91c56667
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-05 16:45:25 +0000

    added packages

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// INQUIRER
+
+// view
+// - all departments        -> display departments and ids
+// - all roles              -> role title, role id, department and salary (join departments and role tables)
+// - all employees          -> employee id, first name last name, role, department, salary, and manager (join departments and role tables) has foreign key linked to self in employee table
+
+// add
+// - add departments
+// - add role
+// - add employee
+
+// update
+//  - update employee
